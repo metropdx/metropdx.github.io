25b233e3e84a060fe651596364cdc9c3f492dcd8
Tauren Mills <tauren@sportzing.com>
2016-12-22 00:20:49 -0800

    Added countdown feature

--- js/all.js
+++ js/all.js
@@ -8598,3 +8598,51 @@ $.fn.gmap3 = function () {
     });
 
 })(jQuery);
+
+
+(function($) {
+  function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': t <= 0 ? 0 : days,
+      'hours': t <= 0 ? 0 : hours,
+      'minutes': t <= 0 ? 0 : minutes,
+      'seconds': t <= 0 ? 0 : seconds
+    };
+  }
+
+  function initialize(el, endtime) {
+    // var clock = document.getElementById(id);
+    var clock = el;
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+
+  $('[countdown]').each(function() {
+    initialize(this, this.getAttribute('countdown'))
+  });
+
+})(jQuery);
